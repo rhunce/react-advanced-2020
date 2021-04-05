96f73b5a79935c603da4e016446e9d17bb859aff
Raymond Hunce <hunce18c@gmail.com>
2021-04-04 21:37:32 -0400

    Complete 1-useState 2-useState-basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/1-error-example.js';
-// import Final from './tutorial/1-useState/final/1-error-example.js';
+import Setup from './tutorial/1-useState/setup/2-useState-basics';
+import Final from './tutorial/1-useState/final/2-useState-basics';
 
 function App() {
   return (
     <div className='container'>
       <Setup />
-      {/* <Final /> */}
+      <Final />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  let [title, setTitle] = useState('random title');
+
+  const clickHandler = () => {
+    title === 'random title'
+      ? setTitle('Hello World')
+      : setTitle('random title');
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button type='button' className='btn' onClick={clickHandler}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
