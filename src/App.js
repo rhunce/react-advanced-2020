b4c47aaaae9d485b7d93b467360a81d0e5f79df1
Raymond Hunce <hunce18c@gmail.com>
2021-04-04 20:51:58 -0400

    Project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example.js';
+// import Final from './tutorial/1-useState/final/1-error-example.js';
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+      {/* <Final /> */}
+    </div>
+  );
+}
+
+export default App;
