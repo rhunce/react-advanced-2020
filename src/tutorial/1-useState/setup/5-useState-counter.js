bd19559c9860a8ab7cf323d05c228165209eb6c7
Raymond Hunce <hunce18c@gmail.com>
2021-04-05 21:34:10 -0400

    Complete 1-useState 5-useState-counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,53 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [counter, setCounter] = useState(0);
+
+  function decreaseHandler() {
+    let count = counter - 1;
+    setCounter(count);
+  }
+  function resetHandler() {
+    setCounter(0);
+  }
+  function increaseHandler() {
+    let count = counter + 1;
+    setCounter(count);
+  }
+  function complexIncreaseHandler() {
+    // let count = counter + 1;
+    // setTimeout(() => setCounter(count), 2000);
+    setTimeout(() => {
+      setCounter((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  }
+
+  return (
+    <>
+      <div style={{ margin: '4rem 0' }}>
+        <h2>Regular Counter</h2>
+        <h1>{counter}</h1>
+        <button className='btn' onClick={decreaseHandler}>
+          Decrease
+        </button>
+        <button className='btn' onClick={resetHandler}>
+          Reset
+        </button>
+        <button className='btn' onClick={increaseHandler}>
+          Increase
+        </button>
+      </div>
+      <div style={{ margin: '4rem 0' }}>
+        <h2>More Complex Counter</h2>
+        <h1>{counter}</h1>
+        <button className='btn' onClick={complexIncreaseHandler}>
+          Increase Later
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default UseStateCounter;
