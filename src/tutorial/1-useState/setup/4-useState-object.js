1d6c7556f2684f7c5c1ecddbbffb37fb653afb62
Raymond Hunce <hunce18c@gmail.com>
2021-04-05 20:00:35 -0400

    Complete 1-useState 4-useState-object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    message: 'Random Message',
+  });
+
+  const changeMessage = () => {
+    /*
+    setPerson({
+      name: 'Peter',
+      age: 24,
+      message: 'Hello World',
+    });
+    */
+    // OR
+    setPerson({ ...person, message: 'Hello World' });
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>{person.message}</h4>
+      <button className='btn' onClick={changeMessage}>
+        Change Message
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
