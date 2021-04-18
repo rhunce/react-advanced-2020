6d58967ba0fb522fe4a16e0a72c1b7f3d59810f7
Raymond Hunce <hunce18c@gmail.com>
2021-04-18 17:40:34 -0400

    Complete 8-useContext 1-context-api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling.js';
-import Final from './tutorial/7-prop-drilling/final/1-prop-drilling.js';
+import Setup from './tutorial/8-useContext/setup/1-context-api.js';
+import Final from './tutorial/8-useContext/final/1-context-api.js';
 
 function App() {
   return (
--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,38 +3,41 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+// create the context
+const PersonContext = React.createContext();
+// this gives us two components: Provider, Consumer
+// with the arrival of useContext, we won't use Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
+
   const removePerson = (id) => {
     setPeople((people) => {
       return people.filter((person) => person.id !== id);
     });
   };
+
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>Context API / useContext Hook</h3>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const { people } = useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext);
   return (
     <div className='item'>
       <h4>{name}</h4>
